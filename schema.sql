29653519a7765cc06ee318dd399c4cc9461a677e
SHALINI K <k.shalinikumar26@gmail.com>
2026-09-04 23:16:55 +0530

    Add Books table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,13 @@ CREATE TABLE Authors (
     author_name VARCHAR(100) NOT NULL,
     nationality VARCHAR(50)
 );
+-- Books table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    genre VARCHAR(50),
+    author_id INT,
+    total_copies INT DEFAULT 1,
+    available_copies INT DEFAULT 1,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
